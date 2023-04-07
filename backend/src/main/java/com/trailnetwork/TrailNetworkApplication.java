cc791d051a36a1ea4c62dbf010d6a96aaeb9e8e2
Max2000s <max.schoellkopf@gmx.de>
2023-04-07 23:23:35 +0200

    initial commit with code

---
+++ backend/src/main/java/com/trailnetwork/TrailNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.trailnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailNetworkApplication.class, args);
+	}
+
+}
