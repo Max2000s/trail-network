f34f2ceab9b7acb343f5ff424264b576bf1a7d8e
Max2000s <max.schoellkopf@gmx.de>
2023-04-09 20:47:21 +0200

    minor content changes due to testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo"  src="./assets/pic.png">
+  <HelloWorld msg="Welcome to TrailNetwork"/>
 </template>
 
 <script>
