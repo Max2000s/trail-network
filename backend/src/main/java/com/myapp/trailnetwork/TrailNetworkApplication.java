87b0cb9f6d8fc69da6fb8117f683d48f0688f42c
Max2000s <max.schoellkopf@gmx.de>
2023-04-09 21:00:47 +0200

    moved file

--- backend/src/main/java/com/myapp/trailnetwork/TrailNetworkApplication.java
+++ backend/src/main/java/com/myapp/trailnetwork/TrailNetworkApplication.java
@@ -1,4 +1,4 @@
-package com.trailnetwork;
+package com.myapp.trailnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
