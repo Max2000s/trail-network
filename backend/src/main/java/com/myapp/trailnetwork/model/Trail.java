aa9e5b342b942222c1b901cace08d708156a8743
Max2000s <max.schoellkopf@gmx.de>
2023-04-12 19:56:44 +0200

    added setters and getters for Trail model

--- backend/src/main/java/com/myapp/trailnetwork/model/Trail.java
+++ backend/src/main/java/com/myapp/trailnetwork/model/Trail.java
@@ -11,4 +11,28 @@ public class Trail {
     private String name;
     private String location;
     private Level level;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Level getlevel() {
+        return this.level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
 }
