10e619689f4c9a8edba6e7b6410cc79c0fb7618e
Max2000s <max.schoellkopf@gmx.de>
2023-04-12 19:59:38 +0200

    added service for creating a trail

--- backend/src/main/java/com/myapp/trailnetwork/service/TrailService.java
+++ backend/src/main/java/com/myapp/trailnetwork/service/TrailService.java
@@ -1,6 +1,7 @@
 package com.myapp.trailnetwork.service;
 
 import com.myapp.trailnetwork.model.Trail;
+import com.myapp.trailnetwork.model.Level;
 import com.myapp.trailnetwork.repository.TrailRepository;
 
 import java.util.List;
@@ -18,7 +19,15 @@ public class TrailService {
     // Add methods for handling CRUD operations and other business logic
 
     public List<Trail> findAll() {
-        return new List<Trail>();
+        return null;
+    }
+
+    public Trail createTrail(String name, String location, Level level) {
+        Trail newTrail = new Trail();
+        newTrail.setName(name);
+        newTrail.setLocation(location);
+        newTrail.setLevel(level);
+        return this.trailRepository.save(newTrail);
     }
 }
 
