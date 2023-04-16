701feeaf4c6afa9145638dc48aac9fa265f80401
Max2000s <max.schoellkopf@gmx.de>
2023-04-16 16:36:38 +0200

    added mappings and methods for CRUD operations

--- backend/src/main/java/com/myapp/trailnetwork/controller/UserController.java
+++ backend/src/main/java/com/myapp/trailnetwork/controller/UserController.java
@@ -2,9 +2,11 @@ package com.myapp.trailnetwork.controller;
 
 import com.myapp.trailnetwork.model.User;
 import com.myapp.trailnetwork.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -15,22 +17,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
+    @GetMapping("/")
     public List<User> getAllUsers() {
         return userService.findAll();
     }
-
-    // creating a new user
-    @PostMapping
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        Optional<User> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    @PostMapping("/")
     public User createNewUser(String userName, String email, String password) {
         return userService.createUser(userName, email, password);
     }
-
-    // update user name
-    @PostMapping
-    public User upddateUser(String id, String newUserName) {
+    @PostMapping("/{id}")
+    public User updateUserName(String id, String newUserName) {
         return userService.updateUser(id, newUserName);
     }
-
-    // Add more request mappings for other CRUD operations
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteById(id);
+    }
 }
